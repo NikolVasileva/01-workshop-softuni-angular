c538b4650bcad38c91d8fd4c08ceabd785ac9dc9
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-08 20:31:44 +0200

    create get service for post array

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Theme } from "../../shared/interfaces/theme";
+import { Post } from "../../shared/interfaces/post";
 
 @Injectable({
     providedIn: "root"
@@ -14,5 +15,9 @@ export class ApiService {
 
     getThemes(): Observable<Theme[]> {
         return this.http.get<Theme[]>(`${this.apiUrl}/themes`)
+    }
+
+    getLatestPosts(): Observable<Post[]> {
+        return this.http.get<Post[]>(`${this.apiUrl}/posts?limit=5`)
     }
 }
