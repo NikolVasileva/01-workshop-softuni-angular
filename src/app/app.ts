1744d11694e81177cd2bf3c922e01742f0d64874
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-08 16:24:05 +0200

    Add home component to the app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { Home } from './features/home/home';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
