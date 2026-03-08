23c704132f2fc2c7afd2fd6c2c920e2bcf620200
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-08 15:52:52 +0200

    Create Header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './layout/header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
