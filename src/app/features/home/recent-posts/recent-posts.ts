eb9275a6ddf5f4195f2623d6be0f997e248f9ade
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-08 21:23:32 +0200

    add post item to post list

--- src/app/features/home/recent-posts/recent-posts.ts
+++ src/app/features/home/recent-posts/recent-posts.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../../../shared/interfaces/post';
 import { ApiService } from '../../../core/services/api.service';
+import { PostItem } from '../../../shared/components/post-item/post-item';
 
 @Component({
   selector: 'app-recent-posts',
-  imports: [],
+  imports: [PostItem],
   templateUrl: './recent-posts.html',
   styleUrl: './recent-posts.css',
 })
